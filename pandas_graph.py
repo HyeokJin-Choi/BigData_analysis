a8f7f2b93ac23bd8d979a0d78ab2d8b74951e286
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2025-03-31 12:37:37 +0900

    Update pandas_graph.py

--- pandas_graph.py
+++ pandas_graph.py
@@ -1,28 +1,25 @@
+쿼리 추출 
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 # 데이터 읽기
 df = pd.read_csv('C:/Users/thdgh/Downloads/dfdata/iris.csv')
 
+# 품종 리스트
+species_list = df['Species'].unique()
 
-# 화면 분할 정의
-fig, axes = plt.subplots(nrows=2, ncols=2)
+# 서브플롯 생성 (1행 3열)
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
 
+# query() 사용해서 품종별 KDE 그래프
+for ax, species in zip(axes, species_list):
+    df.query("Species == @species")['Petal_Length'].plot.kde(ax=ax)
+    ax.set_title(f'{species} Petal_Length KDE')
+    ax.set_xlabel('Petal Length')
+    ax.set_ylabel('Density')
 
-# 각 분할 영역에 그래프 작성하기
-df['Petal_Length'].plot.hist(ax=axes[0,0])
-df['Petal_Length'].plot.box(ax=axes[0,1])
+# 전체 그래프 제목
+fig.suptitle('Petal Length KDE by Species (Using query)', fontsize=14)
 
-
-fd = df['Species'].value_counts()
-
-
-fd.plot.pie(ax=axes[1,0])
-fd.plot.barh(ax=axes[1,1])
-
-# 통합 그래프에 제목 지정
-fig.suptitle('Multiple Graph Example', fontsize=14)
-
-# 분할 그래프 화면에 나타내기
+plt.tight_layout()
 plt.show()
