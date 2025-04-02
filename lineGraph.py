e6eb179be5b21d16e2d5d65acc1df5dd7f7d3892
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2025-04-02 12:42:59 +0900

    Update lineGraph.py

--- lineGraph.py
+++ lineGraph.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 late = pd.Series([5,8,7,9,4,6,12,13,8,6,6,4],
-index=list(range(1,13)))
+                 index=list(range(1,13)))
 late.plot(title='Late student per month', # 제목
  xlabel= 'month', # x축 레이블
  ylabel='frequency', # y축 레이블
- linestyle='solid') # 선의 종류
+ linestyle='solid') # 선의 종류, 실선
 plt.show()
+
 late.plot(title='Late student per month', # 제목
  xlabel= 'month', # x축 레이블
  ylabel='frequency', # y축 레이블
- linestyle='solid', # 선의 종류
+ linestyle='dashed', # 선의 종류, 점선
  marker='o') # 점의 종류
 plt.show()
