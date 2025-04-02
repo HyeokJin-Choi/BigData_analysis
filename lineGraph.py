0bd74618c798aeac6d1370eb043a3c3db9c32422
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2025-04-02 12:48:34 +0900

    Update lineGraph.py

--- lineGraph.py
+++ lineGraph.py
@@ -14,3 +14,16 @@ late.plot(title='Late student per month', # 제목
  linestyle='dashed', # 선의 종류, 점선
  marker='o') # 점의 종류
 plt.show()
+
+late1 = [5,8,7,9,4,6,12,13,8,6,6,4]
+late2 = [4,6,5,8,7,8,10,11,6,5,7,3]
+dict = {'class_1': late1, 'class_2': late2}
+late = pd.DataFrame(dict,
+index=list(range(1,13)))
+late
+late.plot(title='Late student per month', # 제목
+ xlabel= 'month', # x축 레이블
+ ylabel='frequency', # y축 레이블
+ marker='o') # 점의 종류
+plt.plt.legend(loc='upper right') # 범례 지정
+plt.show()
