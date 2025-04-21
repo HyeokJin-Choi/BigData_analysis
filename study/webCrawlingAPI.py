333b6bf9c40b54e2462908ea722562f4f1bd5769
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2025-04-21 14:32:02 +0900

    Update webCrawlingAPI.py

--- study/webCrawlingAPI.py
+++ study/webCrawlingAPI.py
@@ -1,13 +1,12 @@
 import requests
 import pandas as pd
 
-def test(year=2020, age_list=[14, 20, 30], rank=10):
-    df14, df20, df30 = None, None, None
+def webAPI(year=2020, age_list=[14, 20, 30], rank=10):
     columnsList = ['대출순위', '제목', '작가', '출판사', '대출회수']
     raw_columns = ['no', 'bookname', 'authors', 'publisher', 'loan_count']
-    age_index = 0
+    result = []
 
-    for a in age:
+    for a in age_list:
         url = f"http://data4library.kr/api/loanItemSrch?authKey=92d0610b51b298034f363dbd3f366ba1c0415a0bb17dd1c0ac5dc5f40f6d5b04&startDt={year}-01-01&endDt={year}-12-31&age={a}&format=json"
         r = requests.get(url)
         data = r.json()
@@ -27,10 +26,11 @@ def test(year=2020, age_list=[14, 20, 30], rank=10):
         df = pd.DataFrame(books, columns=raw_columns)
         df.columns = columnsList
 
-        print(df)
+        result.append(df)
+    return result
 
-
-
+# 메인 실행 코드
+print("*** 도서관 정보나루의 연도, 연령별 인기대출도서에 대한 분석 프로그램입니다. ***")
 year = int(input("확인할 연도 입력: "))
 n = int(input("확인하고 싶은 연령의 개수: "))
 age = []
@@ -41,5 +41,8 @@ for i in range(n):
 
 rank = int(input("몇 위까지 확인하시겠습니까? (ex. 10): "))
 
+result = webAPI(year=year, age_list=age, rank=rank)
 
-df_by_age = test(year=year, age_list=age, rank=rank)
+for d_i in range(n):
+    print(f"\n{year}년도, {age[d_i]}세에게 인기가 많았던 상위 {rank}권의 도서입니다.\n")
+    print(result[d_i])
