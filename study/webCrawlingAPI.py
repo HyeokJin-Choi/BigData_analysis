0b066668b479964fc7e204f7ba6edbefdaf52478
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2025-04-21 14:49:40 +0900

    Update webCrawlingAPI.py

--- study/webCrawlingAPI.py
+++ study/webCrawlingAPI.py
@@ -1,13 +1,25 @@
 import requests
 import pandas as pd
+import calendar # 정확한 날짜의 계산을 위해서 사용
 
-def webAPI(year=2020, age_list=[14, 20, 30], rank=10):
+def webAPI(year=2020, month=1, age_list=[14, 20, 30], rank=10):
     columnsList = ['대출순위', '제목', '작가', '출판사', '대출회수']
     raw_columns = ['no', 'bookname', 'authors', 'publisher', 'loan_count']
     result = []
 
+    # 월과 마지막 날짜 계산
+    month_str = str(month).zfill(2)  # '01', '02', ..., '12' <- 월은 9월달 이하면 숫자 앞에 0을 붙여야함.
+    end_day = calendar.monthrange(year, month)[1]  # 정확한 말일자 계산 <- 예시로 2월은 28일까지 있음. 무조건 31일이 아님.
+
     for a in age_list:
-        url = f"http://data4library.kr/api/loanItemSrch?authKey=92d0610b51b298034f363dbd3f366ba1c0415a0bb17dd1c0ac5dc5f40f6d5b04&startDt={year}-01-01&endDt={year}-12-31&age={a}&format=json"
+        url = (
+            f"http://data4library.kr/api/loanItemSrch?"
+            f"authKey=92d0610b51b298034f363dbd3f366ba1c0415a0bb17dd1c0ac5dc5f40f6d5b04"
+            f"&startDt={year}-{month_str}-01"
+            f"&endDt={year}-{month_str}-{end_day}"
+            f"&age={a}&format=json"
+        )
+
         r = requests.get(url)
         data = r.json()
 
@@ -25,24 +37,32 @@ def webAPI(year=2020, age_list=[14, 20, 30], rank=10):
 
         df = pd.DataFrame(books, columns=raw_columns)
         df.columns = columnsList
-
         result.append(df)
+
     return result
 
-# 메인 실행 코드
-print("*** 도서관 정보나루의 연도, 연령별 인기대출도서에 대한 분석 프로그램입니다. ***")
+# *** 메인 실행 코드 ***
+print("*** 도서관 정보나루의 연도, 월, 연령별 인기대출도서 분석 프로그램 ***")
 year = int(input("확인할 연도 입력: "))
+month = int(input(f"{year}년도의 확인할 월 입력 (ex. 1월이면 1, 2월이면 2 ...): "))
 n = int(input("확인하고 싶은 연령의 개수: "))
 age = []
-
 for i in range(n):
     a = int(input(f"{i+1}번째 연령 입력: "))
     age.append(a)
 
 rank = int(input("몇 위까지 확인하시겠습니까? (ex. 10): "))
 
-result = webAPI(year=year, age_list=age, rank=rank)
+result = webAPI(year=year, month=month, age_list=age, rank=rank)
 
 for d_i in range(n):
-    print(f"\n{year}년도, {age[d_i]}세에게 인기가 많았던 상위 {rank}권의 도서입니다.\n")
+    print(f"\n{year}년 {month}월, {age[d_i]}세에게 인기 있었던 상위 {rank}권의 도서입니다.")
     print(result[d_i])
+
+# 과제용
+# 2025 년 14,20,30 대의 (1월) rank 10
+df14 = pd.DataFrame(result[0])
+df20 = pd.DataFrame(result[1])
+df30 = pd.DataFrame(result[2])
+
+
